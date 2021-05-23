8e766a11c6b3eb289fbd04d488ebd8cd8b2ddd46
unknown <jou.098olo@gmail.com>
2021-05-22 21:05:33 -0300

    Adicionando exception de 'objeto não encontrado' corretamente

--- src/main/java/com/joaoh/betha/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/joaoh/betha/resources/exceptions/ResourceExceptionHandler.java
@@ -5,13 +5,14 @@ import com.joaoh.betha.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-
+    @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException exception, HttpServletRequest request) {
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),"Não encontrado" , exception.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
